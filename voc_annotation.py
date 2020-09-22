64ad7f5accfaa92392609051946a68724f1296f1
tfukumori <t_fukumori@mac.com>
2020-09-22 19:03:16 +0900

    Fixed conversion of annotation position values from float to int

--- voc_annotation.py
+++ voc_annotation.py
@@ -18,7 +18,10 @@ def convert_annotation(year, image_id, list_file):
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
-        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
+        b = (int(float(xmlbox.find('xmin').text)),
+             int(float(xmlbox.find('ymin').text)),
+             int(float(xmlbox.find('xmax').text)),
+             int(float(xmlbox.find('ymax').text)))
         list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
 
 wd = getcwd()
